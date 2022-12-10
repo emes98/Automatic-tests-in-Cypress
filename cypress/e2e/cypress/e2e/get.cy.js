95599b03f333683ebeb3cb1638d5e4eae15c98bc
Marta <emslow@wp.pl>
2022-12-10 15:45:44 +0100

    First lines

--- cypress/e2e/cypress/e2e/get.cy.js
+++ cypress/e2e/cypress/e2e/get.cy.js
@@ -3,5 +3,17 @@
 describe("Learning the 'get' method", () => {
   it("get", () => {
     cy.visit("https://example.cypress.io/");
+
+    cy.get(".banner-alt")
+      .should("have.length", 3)
+      .eq(1)
+      .next()
+      .contains("Promise");
+
+    cy.get("ul.home-list").find("li").eq(5);
+    cy.get("ul.home-list").children("li").eq(1).contains("find");
+    cy.get("ul.home-list > li:nth-child(2)");
+
+    cy.get("ul.home-list").contains("Cookies");
   });
 });
